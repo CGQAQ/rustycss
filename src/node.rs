bc60e7d72c103a217343f294dd32029f13035104
CGQAQ <m.jason.liu@outlook.com>
2021-09-25 04:16:39 +0800

    init

---
+++ src/node.rs
@@ -0,0 +1,18 @@
+use crate::datatype::Number;
+
+pub(crate) struct Position {
+    /// Source offset in file. It starts from 0.
+    offset: Number,
+
+    /// Source line in file. In contrast to `offset` it starts from 1.
+    column: Number,
+
+    /// Source column in file.
+    line: Number,
+}
+
+
+
+
+#[test]
+fn test_hello_world() {}
